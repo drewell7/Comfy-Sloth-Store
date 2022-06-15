2e549894c1203d1bba604c2876d817ccf18126a3
drewell7 <91854055+drewell7@users.noreply.github.com>
2022-06-15 12:08:14 -0600

    Home-contact

--- src/components/Services.js
+++ src/components/Services.js
@@ -17,16 +17,16 @@ const Services = () => {
             animi repellat illum!
           </p>
         </article>
-        <div className='services-center'>
+        <div className="services-center">
           {services.map((service) => {
-            const {id, icon, title, text} = service;
+            const { id, icon, title, text } = service;
             return (
-              <article key={id} className='service'>
-                <span className='icon'>{icon}</span>
+              <article key={id} className="service">
+                <span className="icon">{icon}</span>
                 <h4>{title}</h4>
                 <p>{text}</p>
               </article>
-            )
+            );
           })}
         </div>
       </div>
