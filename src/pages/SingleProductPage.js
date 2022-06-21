076dce7a05c139ccdd6cb62ff6330810337aa4a0
drewell7 <91854055+drewell7@users.noreply.github.com>
2022-06-20 20:50:49 -0600

    Work on ProductPage

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -56,36 +56,36 @@ const SingleProductPage = () => {
   return (
     <Wrapper>
       <PageHero title={name} product />
-      <div className='section section-center page'>
-        <Link to='/products' className='btn'>
+      <div className="section section-center page">
+        <Link to="/products" className="btn">
           back to products
         </Link>
-        <div className='product-center'>
-          <ProductImages images={images}/>
-          <section className='content'>
+        <div className="product-center">
+          <ProductImages images={images} />
+          <section className="content">
             <h2>{name}</h2>
             <Stars stars={stars} reviews={reviews} />
-            <h5 className='price'>{formatPrice(price)}</h5>
-            <p className='desc'>{description}</p>
-            <p className='info'>
+            <h5 className="price">{formatPrice(price)}</h5>
+            <p className="desc">{description}</p>
+            <p className="info">
               <span>Available : </span>
-              {stock > 0 ? 'In stock':'out of stock'}
+              {stock > 0 ? "In stock" : "out of stock"}
             </p>
-            <p className='info'>
+            <p className="info">
               <span>SKU : </span>
               {sku}
             </p>
-            <p className='info'>
+            <p className="info">
               <span>Brand : </span>
               {company}
             </p>
             <hr />
-            {stock > 0 && <AddToCart />}
+            {stock > 0 && <AddToCart product={product} />}
           </section>
         </div>
       </div>
     </Wrapper>
-  )
+  );
 };
 
 const Wrapper = styled.main`
